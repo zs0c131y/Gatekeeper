8efac2e3999f2fb693c2660c57664e150b962746
Arden Diago <diagoarden@gmail.com>
2026-02-23 23:11:35 +0530

    added a crash route to the temp project

--- temp_servers/server.js
+++ temp_servers/server.js
@@ -8,8 +8,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/err", (req, res) => {
-    process.exit(1);
-})
+    return 100 / 0
+});
+
+app.get("/crash", (req, res) => {
+
+    const randomNo = Math.floor(
+        Math.random() * (5 - 0) + 5
+    )
+
+    console.log(randomNo);
+
+    if (randomNo % 2 == 0) {
+        process.exit(1);
+    } else {
+        res.send("Server is running");
+    }
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
